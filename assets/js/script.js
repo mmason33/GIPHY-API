7081d77dc6d9c5f1fc266b315daa2c4794e28a8a
michael mason <michael@michaeljmason.com>
2017-07-17 15:17:58 -0700

    adding click to play

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,15 +14,14 @@ function api (searchTerm , page) {
 	}).done(function(result){
 
 		res = result;
-		res.pagination.offset = page;
 		console.log(res);
 
 		for (let i = 0; i < res.data.length; i++) { 
 
 			$('.gifs-returned-row').append(
 				'<div class="col-md-4 text-center">' +
-					`<div class="gif" style="background:url(${res.data[i].images.fixed_height.url}) no-repeat center center; background-size:cover;"></div>` +
-					`<a href="${res.data[i].url} class="gif-link btn btn-primary">View GIF</a>` +
+					`<div class="gif" id="${i}"style="background:url(${res.data[i].images.fixed_height_still.url}) no-repeat center center; background-size:cover;"></div>` +
+					`<a href="${res.data[i].url}" class="gif-link btn btn-primary">View GIF</a>` +
 				'</div>'
 			);
 
@@ -30,6 +29,18 @@ function api (searchTerm , page) {
 		if (res.pagination.offset === 0) {
 			$('.gifs-returned').append('<button class="load btn btn-primary">Load More</button>');
 		}
+
+		$('.gif').click( function () {
+			let id = $(this).attr("id");
+
+			if ($(this).hasClass('playing') === true) {
+				$(this).remove('playing');
+				$(this).css('background', 'url('+ res.data[id].images.fixed_height_still.url +')');
+			} else {
+				$(this).addClass('playing');
+				$(this).css('background', 'url('+ res.data[id].images.fixed_height.url +')');
+			}
+		});	
 	});
 
 }
@@ -41,11 +52,15 @@ $('#gif-button').click( function (e) {
 	let searchTerm = $('#gif-search').val();
 	let page = 0;
 	api(searchTerm, page);
+
+
 	setTimeout( function () {
 
 		$('.load').click( function (e) {
 			e.preventDefault();
-			page += 26;
+			$('.gifs-returned-row').empty();
+			// $('.load').remove();
+			page += 25;
 			api(searchTerm, page)
 
 		});
