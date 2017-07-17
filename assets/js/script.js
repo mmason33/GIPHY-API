1eb88191b79c22cc3abf977bd5f0caf70be76ca1
michael mason <michael@michaeljmason.com>
2017-07-17 13:06:30 -0700

    giphy api working and rendering gif to screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,44 @@
 // api key dc6zaTOxFJmzC
 // host api.giphy.com
 // search cat http://api.giphy.com/v1/stickers/search?q=cat&api_key=dc6zaTOxFJmzC
-var res;
 
-function api () {
 
+function api (searchTerm) {
+
+	let res;
 
 	$.ajax({
-		url: 'http://api.giphy.com/v1/stickers/search?q=cat&api_key=dc6zaTOxFJmzC',
+		url: `http://api.giphy.com/v1/stickers/search?q=${searchTerm}&api_key=dc6zaTOxFJmzC`,
 		method: 'GET'
 
 	}).done(function(result){
 
 		res = result;
 		console.log(res);
+
+		for (let i = 0; i < res.data.length; i++) { 
+
+			$('.gifs-returned-row').append(
+				'<div class="col-md-4">' +
+					`<div class="gif" style="background:url(${res.data[i].images.fixed_height.url}) no-repeat center center; background-size:cover;"></div>` +
+				'</div>'
+			);
+
+		}
+
+
 	});
 
 }
 
-api();
+
+
+$('#gif-button').click( function (e) {
+
+	e.preventDefault();
+	
+	let searchTerm = $('#gif-search').val();
+
+	api(searchTerm);
+
+});
