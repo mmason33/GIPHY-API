a5fc19ba56480ef45b663cafc281aefaf9435485
michael mason <michael@michaeljmason.com>
2017-07-19 09:38:30 -0700

    lazy loading is loading too many

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,67 +7,74 @@ var giphy = {
 	response: [],
 	gifIndex: 0,
 	offset: 0,
-	api: function (){},
-	handleHover: function () {},
-	handleViewClick: function () {},
-	lazyLoad: function () {},
-	smoothScroll: function () {}
-}
+	searchTerm: '',
+	handleSubmit: function (e) {
+		console.log(this);
+		e.preventDefault();
+		$('.gifs-returned-row').empty();
+		giphy.searchTerm = $('#gif-search').val().replace(/ /g, '+');
+		giphy.api(giphy.searchTerm, giphy.offset);
 
-var res = [];
-var resIndex;
-var page = 0;
-function api (searchTerm , page) {
 
-	let query =  `https://api.giphy.com/v1/stickers/search?q=${searchTerm}&api_key=dc6zaTOxFJmzC&offset=${page}`;
-	$.ajax({
-		url: query,
-		method: 'GET'
+	},
 
-	}).done(function(result){
+	api: function (searchTerm, offset) {
 
-		console.log(query);
+		let query =  `https://api.giphy.com/v1/stickers/search?q=${searchTerm}&api_key=dc6zaTOxFJmzC&offset=${offset}`;
+		$.ajax({
+			url: query,
+			method: 'GET'
 
-		for (let i = 0; i < result.data.length; i++) { 
-			res.push(result.data[i]);
-			var indexOF = res.indexOf(result.data[i]);
-			$('.gifs-returned-row').append(
-				'<div class="col-md-4 col-sm-12 text-center">' +
-					`<div class="gif" data-gif="${indexOF}" style="background:url(${res[indexOF].images.fixed_height_still.url}) no-repeat center center; background-size:cover;"></div>` +
-					`<a href="#" class="gif-link-view btn btn-primary">View</a>` +
-					`<a href="${res[indexOF].images.fixed_height.url}" class="gif-link btn btn-primary" download>Download</a>` +
-				'</div>'
-			);
+		}).done(function(result){
 
+			console.log(query);
+
+			for (let i = 0; i < result.data.length; i++) { 
+
+				giphy.response.push(result.data[i]);
+				giphy.gifIndex = giphy.response.indexOf(result.data[i]);
+
+				$('.gifs-returned-row').append(
+					'<div class="col-md-4 col-sm-12 text-center">' +
+						`<div class="gif" data-gif="${giphy.gifIndex}" style="background:url(${giphy.response[giphy.gifIndex].images.fixed_height_still.url}) no-repeat center center; background-size:cover;"></div>` +
+						`<a href="#" class="gif-link-view btn btn-primary">View</a>` +
+						`<a href="${giphy.response[giphy.gifIndex].images.fixed_height.url}" class="gif-link btn btn-primary" download>Download</a>` +
+					'</div>'
+				);
+
+			}
+
+			$('.gif').hover(giphy.handleHover);
+
+			$('.gif-link-view').click(giphy.handleViewClick);
+
+			$(window).scroll(giphy.lazyLoad);
+
+			$(window).scroll(giphy.smoothScroll);
+
+			$('.top').click(giphy.toTop);
+
+
+		});
+
+	},
+
+
+	handleHover: function () {
+
+		let id = $(this).attr('data-gif');
+
+		if ($(this).hasClass('playing') === false && $(this).hasClass('.gif-view') === false) {
+			$(this).addClass('playing');
+			$(this).css({'background':'url('+ giphy.response[id].images.fixed_height.url +') no-repeat center center', 'background-size': 'cover'});
+		} else if ($(this).hasClass('playing') === true && $(this).hasClass('.gif-view') === false) {
+			$(this).removeClass('playing');
+			$(this).css({'background':'url('+ giphy.response[id].images.fixed_height_still.url +') no-repeat center center', 'background-size': 'cover'});
 		}
 
-		$('.load').removeClass('hide');
+	},
 
-
-		$('.gif').hover(handleHover);
-
-		$('.gif-link-view').click(handleViewClick);
-
-
-	});
-
-}
-
-function handleHover() {
-
-	let id = $(this).attr('data-gif');
-
-	if ($(this).hasClass('playing') === false && $(this).hasClass('.gif-view') === false) {
-		$(this).addClass('playing');
-		$(this).css({'background':'url('+ res[id].images.fixed_height.url +') no-repeat center center', 'background-size': 'cover'});
-	} else if ($(this).hasClass('playing') === true && $(this).hasClass('.gif-view') === false) {
-		$(this).removeClass('playing');
-		$(this).css({'background':'url('+ res[id].images.fixed_height_still.url +') no-repeat center center', 'background-size': 'cover'});
-	}
-
-}
-
-function handleViewClick() {
+	handleViewClick: function () {
 
 		$('.gif').off();
 
@@ -77,91 +84,223 @@ function handleViewClick() {
 
 			$(this).parent('.col-md-4').removeClass('col-md-4');
 			$('.col-md-4').fadeOut();
-			$('.load').hide();
 			$('.top').hide();
 			setTimeout(() => {
 
 				$(this).siblings('.gif').addClass('gif-view');
-				$(this).siblings('.gif').css({'background':'url('+ res[id].images.fixed_height.url +') no-repeat center center', 'background-size': 'cover'});
+				$(this).siblings('.gif').css({'background':'url('+ giphy.response[id].images.fixed_height.url +') no-repeat center center', 'background-size': 'cover'});
 				$(this).text('Back');
 
 			}, 500);
 
 		} else {
-
+			$(this).siblings('.gif').css({'background':'url('+ giphy.response[id].images.fixed_height_still.url +') no-repeat center center', 'background-size': 'cover'});
 			$(this).parent().addClass('col-md-4');
 			$(this).siblings('.gif').removeClass('gif-view');
 			$(this).text('View');
 			$('.col-md-4').fadeIn();
-			$('.load').show();
 			$('.top').show();
 
 		    $('html, body').animate({
 		        scrollTop: $(this).parent().offset().top
 		    }, 600);
 
-			$('.gif').hover(handleHover);
+			$('.gif').hover(giphy.handleHover);
 
 		}	
 
-}
+	},
 
-$(document).ready(function () {
+	lazyLoad: function () {
 
-	$('#gif-button').click( function (e) {
-		e.preventDefault();
-		$('.gifs-returned-row').empty();
-		let searchTerm = $('#gif-search').val().replace(/ /g, '+');
-		api(searchTerm, page);
-		console.log(searchTerm);
-
-		
-
-		// $('.load').click( function (e) {
-
-		// 	e.preventDefault();
-		// 	page += 25;
-		// 	api(searchTerm, page)
-
-		// });
-
-		$(window).scroll( function () {
-
-			var hT = $('.gif').last().offset().top;
-			var hH = $('.gif').last().outerHeight();
-			var wH = $(window).height();
-			var wS = $(this).scrollTop();
+		var hT = $('.gif').last().offset().top;
+		var hH = $('.gif').last().outerHeight();
+		var wH = $(window).height();
+		var wS = $(this).scrollTop();
 
 
-			if (wS > (hT + hH - wH)) {
-				console.log('worked');
-				e.preventDefault();
-				page += 25;
-				api(searchTerm, page)
+		if (wS > (hT + hH - wH)) {
+			console.log('worked', wS > (hT + hH - wH));
+			giphy.offset += 25;
+			giphy.api(giphy.searchTerm, giphy.offset)
 
-			}
+		}
 
+	},
+	smoothScroll: function () {
 
-		});
-		
-	});
+	        if ($(this).scrollTop() > 100) {
+	            $('.top').fadeIn();
+	        } else {
+	            $('.top').fadeOut();
+	        }
 
+	},
 
-	// smooth scroll back to top
+	toTop: function () {
 
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.top').fadeIn();
-        } else {
-            $('.top').fadeOut();
-        }
-    });
-
-	$('.top').click(function(){
+			
         $("html, body").animate({
             scrollTop: 0
         }, 1000);
-    });   
+		      		
+
+	}
+}
+
+
+
+
+// var res = [];
+// var resIndex;
+// var page = 0;
+// function api (searchTerm , page) {
+
+// 	let query =  `https://api.giphy.com/v1/stickers/search?q=${searchTerm}&api_key=dc6zaTOxFJmzC&offset=${page}`;
+// 	$.ajax({
+// 		url: query,
+// 		method: 'GET'
+
+// 	}).done(function(result){
+
+// 		console.log(query);
+
+// 		for (let i = 0; i < result.data.length; i++) { 
+// 			res.push(result.data[i]);
+// 			var indexOF = res.indexOf(result.data[i]);
+// 			$('.gifs-returned-row').append(
+// 				'<div class="col-md-4 col-sm-12 text-center">' +
+// 					`<div class="gif" data-gif="${indexOF}" style="background:url(${res[indexOF].images.fixed_height_still.url}) no-repeat center center; background-size:cover;"></div>` +
+// 					`<a href="#" class="gif-link-view btn btn-primary">View</a>` +
+// 					`<a href="${res[indexOF].images.fixed_height.url}" class="gif-link btn btn-primary" download>Download</a>` +
+// 				'</div>'
+// 			);
+
+// 		}
+
+// 		$('.load').removeClass('hide');
+
+
+// 		$('.gif').hover(handleHover);
+
+// 		$('.gif-link-view').click(handleViewClick);
+
+
+// 	});
+
+// }
+
+// function handleHover() {
+
+// 	let id = $(this).attr('data-gif');
+
+// 	if ($(this).hasClass('playing') === false && $(this).hasClass('.gif-view') === false) {
+// 		$(this).addClass('playing');
+// 		$(this).css({'background':'url('+ res[id].images.fixed_height.url +') no-repeat center center', 'background-size': 'cover'});
+// 	} else if ($(this).hasClass('playing') === true && $(this).hasClass('.gif-view') === false) {
+// 		$(this).removeClass('playing');
+// 		$(this).css({'background':'url('+ res[id].images.fixed_height_still.url +') no-repeat center center', 'background-size': 'cover'});
+// 	}
+
+// }
+
+// function handleViewClick() {
+
+// 		$('.gif').off();
+
+// 		let id = $(this).siblings('.gif').attr('data-gif');
+
+// 		if ($(this).siblings('.gif').hasClass('gif-view') === false) {
+
+// 			$(this).parent('.col-md-4').removeClass('col-md-4');
+// 			$('.col-md-4').fadeOut();
+// 			$('.load').hide();
+// 			$('.top').hide();
+// 			setTimeout(() => {
+
+// 				$(this).siblings('.gif').addClass('gif-view');
+// 				$(this).siblings('.gif').css({'background':'url('+ res[id].images.fixed_height.url +') no-repeat center center', 'background-size': 'cover'});
+// 				$(this).text('Back');
+
+// 			}, 500);
+
+// 		} else {
+
+// 			$(this).parent().addClass('col-md-4');
+// 			$(this).siblings('.gif').removeClass('gif-view');
+// 			$(this).text('View');
+// 			$('.col-md-4').fadeIn();
+// 			$('.load').show();
+// 			$('.top').show();
+
+// 		    $('html, body').animate({
+// 		        scrollTop: $(this).parent().offset().top
+// 		    }, 600);
+
+// 			$('.gif').hover(handleHover);
+
+// 		}	
+
+// }
+
+$(document).ready(function () {
+
+	$('#gif-button').click(giphy.handleSubmit);
+
+	// $('#gif-button').click( function (e) {
+	// 	e.preventDefault();
+	// 	$('.gifs-returned-row').empty();
+	// 	let searchTerm = $('#gif-search').val().replace(/ /g, '+');
+	// 	api(searchTerm, page);
+	// 	console.log(searchTerm);
+
+		
+
+	// 	// $('.load').click( function (e) {
+
+	// 	// 	e.preventDefault();
+	// 	// 	page += 25;
+	// 	// 	api(searchTerm, page)
+
+	// 	// });
+
+	// 	$(window).scroll( function () {
+
+	// 		var hT = $('.gif').last().offset().top;
+	// 		var hH = $('.gif').last().outerHeight();
+	// 		var wH = $(window).height();
+	// 		var wS = $(this).scrollTop();
+
+
+	// 		if (wS > (hT + hH - wH)) {
+	// 			console.log('worked');
+	// 			e.preventDefault();
+	// 			page += 25;
+	// 			api(searchTerm, page)
+
+	// 		}
+
+
+	// 	});
+		
+	// });
+
+
+	// // smooth scroll back to top
+
+ //    $(window).scroll(function () {
+ //        if ($(this).scrollTop() > 100) {
+ //            $('.top').fadeIn();
+ //        } else {
+ //            $('.top').fadeOut();
+ //        }
+ //    });
+
+	// $('.top').click(function(){
+ //        $("html, body").animate({
+ //            scrollTop: 0
+ //        }, 1000);
+ //    });   
 
 
 });
