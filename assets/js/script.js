622b6feb1195c0f2ecbd0a68c2f8414dfbd0f6d8
Michael Mason <michael@michaeljmason.com>
2017-07-21 12:52:34 -0700

    spacing

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,7 @@ var giphy = () => {
 		gifIndex: 0,
 		offset: 0,
 		searchTerm: '',
-
 		handleSubmit (e) {
-
 			if ($(this).val() !== '') {
 				gif.offset = 0;
 				e.preventDefault();
@@ -14,24 +12,18 @@ var giphy = () => {
 				gif.searchTerm = $(this).val().replace(/ /g, '+');
 				gif.api(gif.searchTerm, gif.offset);
 			}
-
 		},
 		api (searchTerm, offset) {
-
 			let query =  `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=dc6zaTOxFJmzC&offset=${offset}`;
 			$.ajax({
 				url: query,
 				method: 'GET'
 
 			}).done(function(result){
-
 				console.log(query);
-
 				for (let i = 0; i < result.data.length; i++) { 
-
 					gif.response.push(result.data[i]);
 					gif.gifIndex = gif.response.indexOf(result.data[i]);
-
 					$('.gifs-returned-row').append(
 						'<div class="col-lg-4 col-md-12 text-center animated zoomIn">' +
 							`<div class="gif" data-gif="${gif.gifIndex}" style="background:url(${gif.response[gif.gifIndex].images.fixed_height_still.url}) no-repeat center center; background-size:cover;"></div>` +
@@ -40,21 +32,15 @@ var giphy = () => {
 
 						'</div>'
 					);
-
 				}
 				$('.gif').hover(gif.handleHover);
-
 				$(window).on( 'scroll', gif.lazyLoad);
-
 				$(window).scroll(gif.smoothScroll);
-
 				$('.top').click(gif.toTop);
 			});
 		},
 		handleHover () {
-
 			let id = $(this).attr('data-gif');
-
 			if ($(this).hasClass('playing') === false) {
 				$(this).addClass('playing');
 				$(this).css({'background':'url('+ gif.response[id].images.fixed_height.url +') no-repeat center center', 'background-size': 'cover'});
@@ -62,7 +48,6 @@ var giphy = () => {
 				$(this).removeClass('playing');
 				$(this).css({'background':'url('+ gif.response[id].images.fixed_height_still.url +') no-repeat center center', 'background-size': 'cover'});
 			}
-
 		},
 		lazyLoad () {
 			var hT = $('.gif').last().offset().top;
@@ -70,17 +55,13 @@ var giphy = () => {
 			var wH = $(window).height();
 			var wS = $(window).scrollTop();
 			var diff = (hT + hH) - wH;
-
 			if (wS > diff && $('.gif').hasClass('gif-view') !== true) {
-
 				$(window).off();
 				gif.offset += 25;
 				gif.api(gif.searchTerm, gif.offset)
-
 			}
 		},
 		smoothScroll () {
-
 			if ($(this).scrollTop() > 100) {
 			    $('.top').fadeIn();
 			} else {
@@ -88,20 +69,15 @@ var giphy = () => {
 			}
 		},
 		toTop () {
-
 			$("html, body").stop(true, false).animate({
 			    scrollTop: 0
 			}, 1000);
-
 		}
-
 	}	
 }
 
 const gif = giphy();
 
 $(document).ready(function () {
-
 	$('#gif-button').click(gif.handleSubmit);
-
 });
